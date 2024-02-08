5d5d148670b92dc58ba05f5f404ca1a508b873a8
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-07 21:56:06 -0500

    add
    hero card

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import "./Hero.css";
-import background from "../../assets/hero-background.jpg";
 import arrow_icon from "../../assets/arrow_icon.png";
 
-import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 
 const Hero = () => {
- 
-    return(
-   <div class="hero">
-    <Card.Body>
-        <Card.Title>Magandang Araw, Good Day!</Card.Title>
-         <Card.Subtitle className="mb-2 text-muted">Welcome to Tahanan Candle Company</Card.Subtitle>
-        <Card.Text>
-        I'm Jez, owner and creator of Tahanan Candle Company.Filipina-ownded with scents inspired by the Philippines
-        </Card.Text>
-        <p>Learn more about the inspiration behind Tahanan Candle Co.</p>
-        <Card.Link class="arrow" href="#about"><img src={arrow_icon} alt="arrow" /></Card.Link>
-      </Card.Body>
-</div> 
-    );
-}
-  
-  export default Hero;
+  return (
+    <div className="hero">
+      <Card className="hero-card" style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title className="title" >Magandang Araw, Good Day!</Card.Title>
+          <Card.Subtitle className="mb-2">
+            Welcome to Tahanan Candle Company
+          </Card.Subtitle>
+          
+          <p>Learn more about the inspiration behind Tahanan Candle Co.</p>
+          <Card.Link className="arrow" href="#about">
+            <img src={arrow_icon} alt="arrow" />
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Hero;
