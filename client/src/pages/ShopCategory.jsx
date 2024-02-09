eaf7c4e719245ab8cd98ff4f9bb314c0c69f1e1e
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-08 22:46:08 -0500

    remove
    nav on bundles page

--- client/src/pages/ShopCategory.jsx
+++ client/src/pages/ShopCategory.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Nav from '../components/Nav/Navbar'
+
 import Header from '../components/Header/Header'
 
 
 const ShopCategory = () => {
   return (
     <>
-      <Nav />
+    
       <Header pageName={"Shop Bundles"} />
       <div>ShopCategory</div>
     </>
