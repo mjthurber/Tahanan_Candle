2c2b703c7c4e0b4ca356779765e0faa2e80471ac
Maggie McCausland <maggiemccausland@hotmail.com>
2024-02-12 15:59:01 -0700

    signup page working

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,72 +1,100 @@
 import Nav from '../components/Nav/Navbar';
-import wave from '../assets/signup-hi.jpg'
+import wave from '../assets/signup-hi.jpg';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import './signup.css';
 
 import React from 'react';
 import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBCardImage,
-  MDBInput,
-  MDBIcon,
-  MDBCheckbox
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBInput,
+    MDBCheckbox
 }
-from 'mdb-react-ui-kit';
+    from 'mdb-react-ui-kit';
 
-function Signup() {
-  return (
-    <>
-    <Nav />
-    <MDBContainer className='sign' fluid>
+function Signup(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [addUser] = useMutation(ADD_USER);
 
-          <MDBCard className='text-black m-5' style={{ borderRadius: '25px' }}>
-              <MDBCardBody>
-                  <MDBRow>
-                      <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const mutationResponse = await addUser({
+            variables: {
+                email: formState.email,
+                password: formState.password,
+                firstName: formState.firstName,
+                lastName: formState.lastName,
+            },
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
 
-                          <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Create an account</p>
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
-                          <div className="d-flex flex-row align-items-center mb-4 ">
-                              <MDBIcon fas icon="user me-3" size='lg' />
-                              <MDBInput label='Your Name' id='form1' type='text' className='w-100' />
-                          </div>
+    return (
+        <>
+            <Nav />
+            <MDBContainer className='sign' style={{ margin: "auto", textAlign: "center", width: "98%", padding: "2%" }}>
 
-                          <div className="d-flex flex-row align-items-center mb-4">
-                              <MDBIcon fas icon="envelope me-3" size='lg' />
-                              <MDBInput label='Your Email' id='form2' type='email' />
-                          </div>
+                <MDBCard className="text-black" style={{ borderRadius: '25px', width: "100%", maxWidth: "900px", margin: "auto", padding: "2%"}}>
+                <Link to="/login" className='text-black'>← Have an account already? <br></br> Go to Login</Link>
 
-                          <div className="d-flex flex-row align-items-center mb-4">
-                              <MDBIcon fas icon="lock me-3" size='lg' />
-                              <MDBInput label='Password' id='form3' type='password' />
-                          </div>
+                    <MDBCardBody>
+                        <MDBRow>
+                            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+                                <form onSubmit={handleFormSubmit}>
+                                    <div className="signup-divs flex-row space-between my-2">
+                                        <label htmlFor="firstName">First Name:</label>
+                                        <MDBInput name="firstName" id='firstName' className='w-100' type="firstName" placeholder="First Name" onChange={handleChange} />
+                                    </div>
+                                    <div className="signup-divs flex-row space-between my-2">
+                                        <label htmlFor="lastName">Last Name:</label>
+                                        <MDBInput name="lastName" id='lastName' className='w-100' type="lastName" placeholder="Last Name" onChange={handleChange} />
+                                    </div>
+                                    <div className="signup-divs flex-row space-between my-2">
+                                        <label htmlFor="email">Email:</label>
+                                        <MDBInput name="email" id='email' className='w-100' type="email" placeholder="youremail@test.com" onChange={handleChange} />
+                                    </div>
+                                    <div className="signup-divs flex-row space-between my-2">
+                                        <label htmlFor="pwd">Password:</label>
+                                        <MDBInput name="password" id='pwd' className='w-100' type="password" placeholder="******" onChange={handleChange} />
+                                    </div>
 
-                          <div className="d-flex flex-row align-items-center mb-4">
-                              <MDBIcon fas icon="key me-3" size='lg' />
-                              <MDBInput label='Repeat your password' id='form4' type='password' />
-                          </div>
+                                    <div className='mb-4'>
+                                        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+                                    </div>
 
-                          <div className='mb-4'>
-                              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
-                          </div>
+                                    <div className="flex-row flex-end">
+                                        <MDBBtn className='mb-4' size='lg' onSubmit={handleFormSubmit}>Register</MDBBtn>
+                                    </div>
+                                </form>
+                            </MDBCol>
 
-                          <MDBBtn className='mb-4' size='lg'>Register</MDBBtn>
-
-                      </MDBCol>
-
-                      <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-                          <MDBCardImage src={wave} fluid />
-                      </MDBCol>
-
-                  </MDBRow>
-              </MDBCardBody>
-          </MDBCard>
-
-      </MDBContainer></>
-  );
+                            <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+                                <MDBCardImage src={wave} fluid />
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBContainer>
+        </>
+    );
 }
 
 export default Signup;
