cbb5f73496a0bf1511f739ee3043a75b28fe2990
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-12 23:06:57 -0500

    styling
    hero welcome message

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "./Hero.css";
 import arrow_icon from "../../assets/arrow_icon.png";
-
-import Image from "react-bootstrap/Image";
 import Card from "react-bootstrap/Card";
 
 const Hero = () => {
   return (
     <div className="hero">
       <Card className="hero-card" id="hero-card-styling">
-        <Card.Body>
+        <Card.Body style={{height: '400px'}}>
           <Card.Title className="title" >Magandang Araw, Good Day!</Card.Title>
           <Card.Subtitle className="mb-2">
             Welcome to Tahanan Candle Company
           </Card.Subtitle>
           
-          <p>Learn more about the inspiration behind Tahanan Candle Co.</p>
+          <p className="p1">Learn more about the inspiration</p>
+          <p>behind Tahanan Candle Co.</p>
           <Card.Link className="arrow" href="about">
             <img src={arrow_icon} alt="arrow" />
           </Card.Link>
