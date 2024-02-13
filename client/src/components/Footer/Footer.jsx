5cca1f397c055537288388a5b06cb5b5793dcca2
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-12 21:27:04 -0500

    add
    footer information and policy links

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,74 +1,108 @@
-import React from 'react'
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow
-} from 'mdb-react-ui-kit';
+import React from "react";
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 
 const Footer = () => {
-    return (
-        <MDBFooter bgColor='light' className='text-center text-lg-left'>
-          <MDBContainer className='p-4'>
-            <MDBRow>
-              <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-                <h5 className='text-uppercase'>Privacy Policy</h5>
+  return (
+        <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+          <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+            <div className='me-5 d-none d-lg-block'>
+              <span>Get connected with us on social networks:</span>
+            </div>
     
-                <ul className='list-unstyled mb-0'>
-                  <li>
-                    <a href='#!' className='link-success'>
-                      "This Privacy Policy describes how TahananCandleCo..."
+            <div>
+              <a href='https://www.instagram.com/tahanan_candle_co/' className='me-4 text-reset'>
+                <MDBIcon fab icon="instagram" />
+              </a>
+              
+            </div>
+          </section>
+    
+          <section className=''>
+            <MDBContainer className='text-center text-md-start mt-5'>
+              <MDBRow className='mt-3'>
+                <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+                  <h6 className='text-uppercase fw-bold mb-4'>
+                    <MDBIcon icon="fire" className="me-3" />
+                    Tahanan Candle Company
+                  </h6>
+                  <p>
+                    Est. 2021??
+                  </p>
+                </MDBCol>
+    
+                <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+                  <h6 className='text-uppercase fw-bold mb-4'>INFO LINKS</h6>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      Privacy Policy
                     </a>
-                  </li>
-                </ul>
-              </MDBCol>
-    
-              <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-                <h5 className='text-uppercase mb-0'>Terms & Conditions</h5>
-    
-                <ul className='list-unstyled'>
-                  <li>
-                    <a href='#!' className='link-success'>
-                      "Please read these Terms of Service carefully before..."
+                  </p>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      Terms & Conditions
                     </a>
-                  </li>
-                </ul>
-              </MDBCol>
-    
-              <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-                <h5 className='text-uppercase'>Shipping Policy</h5>
-    
-                <ul className='list-unstyled mb-0'>
-                  <li>
-                    <a href='#!' className='link-success'>
-                      "Please allow 3-5 days to process your order..."
+                  </p>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      Shipping Policy
                     </a>
-                  </li>
-                </ul>
-              </MDBCol>
-    
-              <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-                <h5 className='text-uppercase mb-0'>Return Policy</h5>
-    
-                <ul className='list-unstyled'>
-                  <li>
-                    <a href='#!' className='link-success'>
-                    "You can always contact us for any question at..."
+                  </p>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      Returns Policy
                     </a>
-                  </li>
-                </ul>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
+                  </p>
+                </MDBCol>
     
-          <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-            &copy; {new Date().getFullYear()} Copyright:{' '}
-            <a className='text-dark' href=''>
-            TahananCandleCo.
+                <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+                  <h6 className='text-uppercase fw-bold mb-4'>FAQs</h6>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      What?
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      Can I?
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      Help!
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' className='text-reset'>
+                      How?
+                    </a>
+                  </p>
+                </MDBCol>
+    
+                <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+                  <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+                  <p>
+                    <MDBIcon icon="home" className="me-2" />
+                    Salt Lake City, Utah, US
+                  </p>
+                  <p>
+                    <MDBIcon icon="envelope" className="me-3" />
+                    contact.us@tahanancandleco.com
+                  </p>
+                  <p>
+                    <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+                  </p>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </section>
+    
+          <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+            © 2021 Copyright:
+            <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+              Tahanan Candle Co.
             </a>
           </div>
         </MDBFooter>
       );
     }
-
     export default Footer;
